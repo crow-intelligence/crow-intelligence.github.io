5b340c14b0d38c0b66aacc5b0cfd3524f76fb33c
zolizoli <zoltan.varju@gmail.com>
2020-01-07 14:16:02 +0100

    fix my bug

--- osszes/lakossag.js
+++ osszes/lakossag.js
@@ -1,4 +1,4 @@
-var margin_020301 = {top: window.innerHeight/20, right: window.innerWidth/20, bottom: window.innerHeight/20, left: window.innerWidth/20};
+var margin_020301 = {top: window.innerHeight/25, right: window.innerWidth/25, bottom: window.innerHeight/25, left: window.innerWidth/25};
 
 var w_020301 = window.innerWidth - margin_020301.left - margin_020301.right - 10;
 var h_020301 = window.innerHeight - margin_020301.top - margin_020301.bottom - 20;
